69d04aa5e2e6a9d3e2495860370e8ad1fe226654
ediltonbs <ediltonbs5@gmail..com>
2022-12-11 20:35:17 -0300

    grafico principal quase pronto

--- code/streamteste.py
+++ code/streamteste.py
@@ -2,25 +2,37 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df = px.data.gapminder().query("year == 2007").query("continent == 'Europe'")
-df.loc[df['pop'] < 2.e6, 'country'] = 'Other countries' # Represent only large countries
-fig = px.pie(df, values='pop', names='country', title='Population of European continent')
-
-df_gols = pd.read_csv("dados_copa.csv")
+df_final = pd.read_csv("df_final.csv")
 st.write("Hello World")
 
+# somatório de gols dados acumulados
+gols_feitos = df_final.groupby(['team','year']).score.sum().unstack().fillna(0).cumsum(axis=1).T.reset_index()
 
-st.line_chart(df)
-st.sidebar.success("Select a demo above.")
+# somatório de gols recebidos acumulados
+gols_tomados = df_final.groupby(['team','year']).conceded.sum().unstack().fillna(0).cumsum(axis=1).T.reset_index()
+
+# somatório das vitórias acumuladas
+vitorias = df_final.groupby(['team','year']).wins.sum().unstack().fillna(0).cumsum(axis=1).T.reset_index()
+
+# somatório das derrota acumuladas
+derrotas = df_final.groupby(['team','year']).losses.sum().unstack().fillna(0).cumsum(axis=1).T.reset_index()
+
 
 #filtro
-choice = st.sidebar.selectbox("Filtrar por", ["gols", "vitórias", "derrotas"])
+choice = st.sidebar.selectbox("Filtrar por", ["Gols dados", "Gols recebidos", "Vitórias", "Derrotas"])
 
 #grafico filtrado
-if choice == "gols":
-    st.line_chart(df_gols)
-else:
-    #st.line_chart(df)
-    fig = px.line(df_gols, x='year', y=df_gols.columns)
-    #st.plotly_chart(fig, use_container_width=True)
+if choice == "Gols dados":
+    fig = px.line(gols_feitos, x='year', y=gols_feitos.columns[1:-1])
+    st.plotly_chart(fig, use_container_width=True)
+elif choice == "Gols recebidos":
+    fig = px.line(gols_tomados, x='year', y=gols_tomados.columns[1:-1])
+    st.plotly_chart(fig, use_container_width=True)
+elif choice == "Vitórias":
+    fig = px.line(vitorias, x='year', y=vitorias.columns[1:-1])
+    st.plotly_chart(fig, use_container_width=True)
+elif choice == "Derrotas":
+    fig = px.line(derrotas, x='year', y=derrotas.columns[1:-1])
+    st.plotly_chart(fig, use_container_width=True)
+
     
