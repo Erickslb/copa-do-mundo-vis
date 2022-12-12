c27fb3cc9ad08a221ab5af9562ea1311a75bd2fb
edilton-bs <ediltonbs5@gmail..com>
2022-12-12 13:56:39 -0300

    tentativa de melhorar a legenda do gráfico

--- code/streamteste.py
+++ code/streamteste.py
@@ -1,5 +1,7 @@
+from pytz import country_names
 import streamlit as st
 import pandas as pd
+import numpy as np
 import plotly.express as px
 
 df_final = pd.read_csv("df_final.csv")
@@ -20,6 +22,11 @@ def plot_filtro(df_final, choice):
     elif choice == "Derrotas":
         df = df_final.groupby(['team','year']).losses.sum().unstack().fillna(0).cumsum(axis=1).T.reset_index()
     fig = px.line(df, x='year', y=df.columns[1:-1])
+    fig.update_traces(hovertemplate='Ano: %{x}<br>' + f'{choice}:' + '%{y}')
+    fig.update_layout(
+    xaxis_title="Ano",
+    yaxis_title=choice,
+    legend_title="País",)
     return st.plotly_chart(fig, use_container_width=False)
 
 ##INTRODUÇÃO##
