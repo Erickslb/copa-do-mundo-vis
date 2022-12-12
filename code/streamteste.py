e27e89706b2c654788215bf703e69c20e903ef0a
edilton-bs <ediltonbs5@gmail..com>
2022-12-12 12:48:44 -0300

    criando função de plotagem

--- code/streamteste.py
+++ code/streamteste.py
@@ -9,18 +9,19 @@ st.set_page_config(page_title="My App",layout='wide')
 
 st.sidebar.write("Sidebar")
 
+# função para aplicar o filtro e plotar o gráfico principal
+def plot_filtro(df_final, choice):
+    if choice == "Gols dados":
+        df = df_final.groupby(['team','year']).score.sum().unstack().fillna(0).cumsum(axis=1).T.reset_index()
+    elif choice == "Gols recebidos":
+        df = df_final.groupby(['team','year']).conceded.sum().unstack().fillna(0).cumsum(axis=1).T.reset_index()
+    elif choice == "Vitórias":
+        df = df_final.groupby(['team','year']).wins.sum().unstack().fillna(0).cumsum(axis=1).T.reset_index()
+    elif choice == "Derrotas":
+        df = df_final.groupby(['team','year']).losses.sum().unstack().fillna(0).cumsum(axis=1).T.reset_index()
+    fig = px.line(df, x='year', y=df.columns[1:-1])
+    return st.plotly_chart(fig, use_container_width=False)
 
-# somatório de gols dados acumulados
-gols_feitos = df_final.groupby(['team','year']).score.sum().unstack().fillna(0).cumsum(axis=1).T.reset_index()
-
-# somatório de gols recebidos acumulados
-gols_tomados = df_final.groupby(['team','year']).conceded.sum().unstack().fillna(0).cumsum(axis=1).T.reset_index()
-
-# somatório das vitórias acumuladas
-vitorias = df_final.groupby(['team','year']).wins.sum().unstack().fillna(0).cumsum(axis=1).T.reset_index()
-
-# somatório das derrota acumuladas
-derrotas = df_final.groupby(['team','year']).losses.sum().unstack().fillna(0).cumsum(axis=1).T.reset_index()
 ##INTRODUÇÃO##
 st.title('TrabViz - Análise das copas')
 
@@ -33,16 +34,4 @@ with col1:
     st.markdown('Esse gráfico tem como objetivo exibir dados acumulados de cada seleção ao longo do tempo: gols feitos, gols tomados, vitórias e derrotas.')    
     choice = st.selectbox ("O que você quer observar ao longo do tempo?", ["Gols dados", "Gols recebidos", "Vitórias", "Derrotas"])
 with col2:
-    #grafico filtrado
-    if choice == "Gols dados":
-        fig = px.line(gols_feitos, x='year', y=gols_feitos.columns[1:-1])
-        st.plotly_chart(fig, use_container_width=False)
-    elif choice == "Gols recebidos":
-        fig = px.line(gols_tomados, x='year', y=gols_tomados.columns[1:-1])
-        st.plotly_chart(fig, use_container_width=False)
-    elif choice == "Vitórias":
-        fig = px.line(vitorias, x='year', y=vitorias.columns[1:-1])
-        st.plotly_chart(fig, use_container_width=False)
-    elif choice == "Derrotas":
-        fig = px.line(derrotas, x='year', y=derrotas.columns[1:-1])
-        st.plotly_chart(fig, use_container_width=False)
+    plot_filtro(df_final, choice)
